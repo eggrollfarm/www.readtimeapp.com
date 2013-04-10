e0ae53597c9d71ef4caba7a4261b4e9676d71d9c
Benjamin Birnbaum <benjamin@wobolando.com>
2013-04-10 21:01:05 +1000

    Cut over from the old site to the new site

--- js/script.js
+++ js/script.js
@@ -1,20 +1,2 @@
 $(document).ready(function() {
-  var showArticle = function() {
-    $(".getting-read").fadeOut(350);
-    $(".good-time").fadeIn(350);
-    $(".fake-phone .article").addClass("onscreen", 400, function() {
-      setTimeout(hideArticle, 3000);
-    });
-  }
-
-  var hideArticle = function() {
-    $(".getting-read").fadeIn(450);
-    $(".good-time").fadeOut(450);
-    $(".fake-phone .article").switchClass("onscreen", "offscreen-left", 400, function() {
-      $(this).removeClass("offscreen-left")
-      setTimeout(showArticle, 3000);
-    });
-  }
-
-  setTimeout(showArticle, 6000);
 });
